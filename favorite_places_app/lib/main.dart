fae4208c62ba9b3c2149c7fbfe68b41bf05c399c
omarzouaoui1 <omarzouaoui100@gmail.com>
2023-07-09 14:50:09 +0100

    Adding the Provider

--- favorite_places_app/lib/main.dart
+++ favorite_places_app/lib/main.dart
@@ -1,7 +1,9 @@
-import 'package:favorite_places_app/screens/places.dart';
 import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:favorite_places_app/screens/places.dart';
 
 final colorScheme = ColorScheme.fromSeed(
   brightness: Brightness.dark,
@@ -28,7 +30,9 @@ final theme = ThemeData().copyWith(
 
 void main() {
   runApp(
-      const MyApp(),
+    const ProviderScope(
+      child: MyApp(),
+    ),
   );
 }
 
