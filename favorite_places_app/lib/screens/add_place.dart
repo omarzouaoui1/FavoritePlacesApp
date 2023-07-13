20562355d1bdce9e25bc60a6c5e6717e7fe70563
omarzouaoui1 <omarzouaoui100@gmail.com>
2023-07-13 12:43:45 +0100

    Get Location input

--- favorite_places_app/lib/screens/add_place.dart
+++ favorite_places_app/lib/screens/add_place.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:favorite_places_app/providers/user_places.dart';
+import 'package:favorite_places_app/widget/location_input.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -65,6 +66,11 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
                   _selectedImage = image;
                 },
               ),
+
+              const SizedBox(height: 10,),
+
+              // Map Input
+              const LocationInput(),
 
               const SizedBox(
                 height: 18,
--- favorite_places_app/lib/widget/location_input.dart
+++ favorite_places_app/lib/widget/location_input.dart
@@ -1,15 +1,51 @@
 import 'package:flutter/material.dart';
 
-class InputLocation extends StatefulWidget {
-  const InputLocation({super.key});
+class LocationInput extends StatefulWidget {
+  const LocationInput({super.key});
 
   @override
-  State<InputLocation> createState() => _InputLocationState();
+  State<LocationInput> createState() => _LocationInputState();
 }
 
-class _InputLocationState extends State<InputLocation> {
+class _LocationInputState extends State<LocationInput> {
   @override
   Widget build(BuildContext context) {
-    return Column();
+    return Column(
+      children: [
+        Container(
+          height: 170,
+          width: double.infinity,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            border: Border.all(
+              width: 2,
+              color: Theme.of(context).colorScheme.primary.withOpacity(0.3),
+            ),
+          ),
+          child: Text(
+            'No location chosen',
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+              color: Theme.of(context).colorScheme.background,
+            ),
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            TextButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.location_on),
+              label: const Text('Get Current Location'),
+            ),
+            TextButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.map),
+              label: const Text('Select on Map'),
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
