82eef0387fc2af3b6182a82c9b8e45c754cee376
omarzouaoui1 <omarzouaoui100@gmail.com>
2023-07-13 12:33:46 +0100

    Location package

---
+++ favorite_places_app/lib/widget/location_input.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class InputLocation extends StatefulWidget {
+  const InputLocation({super.key});
+
+  @override
+  State<InputLocation> createState() => _InputLocationState();
+}
+
+class _InputLocationState extends State<InputLocation> {
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}
